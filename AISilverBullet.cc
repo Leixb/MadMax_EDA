a16593dc8bf8dd769b6d1adbb2a963e02c9088df
LeixB <abone9999@gmail.com>
2018-12-07 21:23:02 +0100

    Implemented cities_map and added DEBUG logging

--- AISilverBullet.cc
+++ AISilverBullet.cc
@@ -1,6 +1,11 @@
 #include "Player.hh"
 #include <list>
 
+#ifdef DEBUG
+#define LOG(x) do { cerr << "debug: " << x << endl; } while(0);
+#else
+#define LOG(x)
+#endif
 
 /**
  * Write the name of your player and save this file
@@ -34,7 +39,10 @@ struct PLAYER_NAME : public Player {
 
     // Global attributes
     dmap water_map, fuel_map;
+
+    dmap nearest_city;
     vector<dmap> cities_map;
+    vector<vector<Pos> > cities;
 
     map<int, Warrior_t> registered_warriors;
     map<int, Car_t> registered_cars;
@@ -43,6 +51,7 @@ struct PLAYER_NAME : public Player {
 
     void bfs(queue<pair<Pos, int> > &q, dmap &m, const bool &cross_city=false);
     void compute_maps();
+    void explore_city(const Pos &p, const int &city, queue<pair<Pos, int> > &q);
 
     list<Dir> get_dir_from_dmap(const Pos &p, const dmap &m);
     void remove_unsafe_dirs(const Unit &u, list<Dir> &l);
@@ -102,18 +111,33 @@ void PLAYER_NAME::init() {
 
 void PLAYER_NAME::compute_maps() {
     queue<pair<Pos, int> > w, f;
+
+    vector<queue<pair<Pos, int> > > cq(nb_cities());
+    nearest_city = dmap(rows(), vector<int> (cols(), INF));
+    cities_map = vector<dmap> (nb_cities(), dmap(rows(), vector<int> (cols(), INF)));
+
+    int city = 0;
+
     for (int i=0; i < rows(); ++i) {
         for (int j=0; j < cols(); ++j) {
             const CellType ct = cell(Pos(i, j)).type;
             if (ct == Water) w.emplace(Pos(i, j), 0);
             else if (ct == Station) f.emplace(Pos(i, j), 0);
             else if (ct == City) {
-                //TODO
+                if (nearest_city[i][j] != INF) continue;
+                explore_city(Pos(i, j), city, cq[city]);
+                ++city;
             }
         }
     }
+
+    LOG("FOUND " << city << "/" << nb_cities() << " cities ");
+
     bfs(w, water_map, true);
     bfs(f, fuel_map, false);
+
+    for (int i = 0; i < nb_cities(); ++i)
+        bfs(cq[i], cities_map[i], true);
 }
 
 void PLAYER_NAME::bfs(queue<pair<Pos, int> > &q, dmap &m, const bool &cross_city) {
@@ -133,6 +157,29 @@ void PLAYER_NAME::bfs(queue<pair<Pos, int> > &q, dmap &m, const bool &cross_city
                 const CellType ct = cell(p2).type;
                 if (ct == Road or ct == Desert or (ct == City and cross_city))
                     q.emplace(p2, d+1);
+            }
+        }
+    }
+}
+
+void PLAYER_NAME::explore_city(const Pos &_p, const int &city, queue<pair<Pos, int> > &q) {
+    queue<Pos> Q;
+    Q.push(_p);
+
+    while (!Q.empty()) {
+        const Pos p = Q.front();
+        Q.pop();
+
+        if (nearest_city[p.i][p.j] != INF) continue;
+        nearest_city[p.i][p.j] = city;
+
+        for (int i = 0; i < DirSize-1; ++i) {
+            const Pos p2 = p + Dir(i);
+            if (pos_ok(p2)) {
+                if (cell(p2).type == City) {
+                    Q.push(p2);
+                    q.emplace(p2, 0);
+                }
             }
         }
     }
