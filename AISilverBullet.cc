d79d02ee37cc1128c3b277f27f6e8c01b2860762
LeixB <abone9999@gmail.com>
2018-12-08 12:55:31 +0100

    Implemented remove unsafe dirs

--- AISilverBullet.cc
+++ AISilverBullet.cc
@@ -31,6 +31,8 @@ struct PLAYER_NAME : public Player {
     // Config
     const int INF=1e8;
 
+    const char CITY_FIGHT_RATIO=75; // Percentatge of probability to win when taking fight
+
     // Types
     struct Warrior_t;
     struct Car_t;
@@ -56,6 +58,10 @@ struct PLAYER_NAME : public Player {
 
     list<Dir> get_dir_from_dmap(const Pos &p, const dmap &m);
     void remove_unsafe_dirs(const Unit &u, list<Dir> &l);
+
+    inline bool fight(const int &attacker_id, const int &victim_id);
+    bool fight_desert(const Unit &attacker_id, const Unit &victim_id);
+    inline bool fight_city(const Unit &attacker_id, const Unit &victim_id);
 
     // Game routines
     void init();
@@ -272,7 +278,56 @@ list<Dir> PLAYER_NAME::get_dir_from_dmap(const Pos &p, const dmap &m) {
     return l;
 }
 
-void PLAYER_NAME::remove_unsafe_dirs(const Unit &u, list<Dir> &l) {}
+void PLAYER_NAME::remove_unsafe_dirs(const Unit &u, list<Dir> &l) {
+    auto it = l.begin();
+    while (it != l.end()) {
+        const Pos p = u.pos + *it;
+        const int u_id = cell(p).id;
+        bool del = false;
+        if (u_id != -1) {
+            if (unit(u_id).player == me()) del=true;
+            else del = !fight(u.id, u_id);
+        }
+        if (!del) {
+            for (int i = 0; i < DirSize-1; ++i) {
+                const Pos p2 = p + Dir(i);
+                const int u_id2 = cell(p2).id;
+                if (pos_ok(p2)) {
+                    if (u_id2 != -1) {
+                        if (unit(u_id2).player != me())
+                            del=!fight(u_id2, u.id);
+                    }
+                }
+            }
+        }
+        if (del) it = l.erase(it);
+        else ++it;
+    }
+}
+
+bool PLAYER_NAME::fight(const int &attacker_id, const int &victim_id) {
+    const Unit attacker=unit(attacker_id), victim=unit(victim_id);
+    if (cell(victim.pos).type == City) return fight_city(attacker, victim);
+    return fight_desert(attacker, victim);
+}
+
+bool PLAYER_NAME::fight_desert(const Unit &attacker, const Unit &victim) {
+    if (victim.type == Car) return false; // Never attack cars
+    if (attacker.type == Car) return true; // Car wins
+    if (victim.food <= 6 or victim.water <= 6) return true;
+
+    if (victim.food > attacker.food and victim.water > attacker.water)
+        return false;
+
+    // TODO: check this is correct
+    if (victim.food < victim.water)
+        return attacker.food > victim.food and attacker.water > victim.food;
+    return attacker.water > victim.water and attacker.food > victim.water;
+}
+
+bool PLAYER_NAME::fight_city(const Unit &attacker, const Unit &victim) {
+    return attacker.water*100/(attacker.water + victim.water) >= CITY_FIGHT_RATIO;
+}
 
 /**
  * Do not modify the following line.
